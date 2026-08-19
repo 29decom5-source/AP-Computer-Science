24db3e6dac7679c7e6d239bf049816701efdaf30
29decom5-source <29decom5@archbishopshaw.us>
2026-08-19 19:57:47 +0000

    y

---
+++ Casting.java
@@ -0,0 +1,8 @@
+public class Casting{
+    void main() {
+        double num1 = 8.2;
+        double num2 = 3.1;
+        int num3 = (int) (num1 + num2 + 0.5);
+        System.out.println(num3);
+    }
+}
--- InputOutput.java
+++ InputOutput.java
@@ -3,10 +3,21 @@ public class InputOutput {
     void main() {
         System.out.println("\n INPUT AND OUTPUT \n");
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is your name");
+        String name = sc.nextLine();
+        System.out.println("hello "+name+ ", How are you?");
+
+        System.out.println("What is your age?");
         int age = sc.nextInt();
         System.out.println("You are "+age+" years old");   
         
-        str name = sc.NextStr();
-        System.out.println("hello "+name+ ", How are you?");
+        System.out.println("How much money do you have?");
+        double money = sc.nextDouble();
+        System.out.println("hello, you have $"+money+"in your account");
+
+    System.out.println("Your name is "+name+". Your age is "+age+". You have $"+money+".");
+
+
     }
 }
