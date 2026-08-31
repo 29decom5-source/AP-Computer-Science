84cb96217e0ebe23b7255c23ec083fdf94882fdd
29decom5-source <29decom5@archbishopshaw.us>
2026-08-31 20:06:52 +0000

    y

--- BankAccount.java
+++ BankAccount.java
@@ -9,15 +9,39 @@ class BankAccount{
         this.balance = balance; //attribute
     }
 
-    public String setowner(){
+    public void setowner(String owner){
+        this.owner = owner;
+    }
+    public void setbalance(double balance){
+        this.balance = balance;
+    }
+
+    public String getowner(){
         return this.owner;
     }
-    public Integer setbalance(){
+    public double getbalance(){
         return this.balance;
     }
 
-    public static void main(){
-       BankAccount account = new BankAccount("Johnson",12112.56);
+    public String toString(){
+        return this.owner + ":" + this.balance;   
+    }
 
+    public static void main(){
+       BankAccount fine = new BankAccount("William Fine",4000000);
+       fine.withdraw(1000000);
+       System.out.println(fine.getbalance());
+       fine.deposit(1000000);
+       fine.setbalance(4000);
+       System.out.println(fine.getbalance());
+       System.out.println(fine);
+
+    }
+
+    public void deposit(double money){
+        this.balance = balance + money;
+    }
+    public void withdraw(double money_take){
+        this.balance = balance - money_take;
     }
 }
