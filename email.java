c1213abdda479cf34ca52b26ad3f3102a9def753
29decom5-source <29decom5@archbishopshaw.us>
2026-09-25 18:43:05 +0000

    y

--- email.java
+++ email.java
@@ -16,14 +16,27 @@ public class email{
 
         int number_first = (int) (Math.random() * period);
         int number_Last_v = Last.length();
-        int number_Last = (int) (Math.random() * number_Last_v -2); //Error has to due with the -2 accidently producing either a negative number or a number too high
-        System.out.println(number_Last_v);
+        int number_Last = (int) (Math.random() * number_Last_v); //Error has to due with the -2 accidently producing either a negative number or a number too high
+        int LongLast = Last.length();
+        System.out.println(number_Last);
+        if (number_Last >= LongLast - 2) {
+            if (number_Last == LongLast - 1) {
+                number_Last -= 2;
+            }
+            else if (number_Last == LongLast - 2) {
+                number_Last -= 1;
+            }
+            else {
+                number_Last -= 3;
+            
+            
+            }
+            }
+        //System.out.println(number_Last_v);
         System.out.println(number_Last);
 
         String user3_1 = First.substring(number_first,number_first+1);
-        String user3_2 = Last.substring(number_Last,number_Last+3);
-
+        String user3_2 = Last.substring(number_Last,number_Last + 3); //error occurs here
         System.out.println(user3_1 + "_" + user3_2);
     }
-
 }
